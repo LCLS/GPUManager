387bb8e80b477e9c4aa0c45d2b59227e5242485f
James Sweet <james.sweet88@googlemail.com>
2016-02-08 11:16:11 -0500

    Added showing added servers

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
 	"text/template"
 
 	"golang.org/x/crypto/ssh"
 )
+
+type Server struct {
+	Hostname, URL string
+	Resources     []Resource
+}
+
+type Resource struct {
+	InUse      bool
+	Name, UUID string
+	Connection *ssh.Session
+}
+
+var servers []Server
 
 func main() {
 	port := flag.Int("port", 8080, "HTTP Server Port")
@@ -24,8 +41,28 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	type ServerInfo struct {
+		Hostname, URL string
+		InUse, Max    int
+		PercentUsed   float64
+	}
+
+	var data []ServerInfo
+
+	for _, server := range servers {
+		info := ServerInfo{Hostname: server.Hostname, URL: server.URL, Max: len(server.Resources)}
+		for _, resource := range server.Resources {
+			if resource.InUse {
+				info.InUse += 1
+			}
+		}
+		info.PercentUsed = (float64(info.InUse) / float64(info.Max)) * 100.0
+
+		data = append(data, info)
+	}
+
 	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, nil)
+	t.Execute(w, data)
 }
 
 func serverAddHandker(w http.ResponseWriter, r *http.Request) {
@@ -67,6 +104,28 @@ func serverAddHandker(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(JSONResponse{false, "Unable to execute command"})
 		return
 	}
+
+	server := Server{Hostname: strings.Split(r.FormValue("server_name"), ".")[0], URL: r.FormValue("server_name")}
+
+	// Find GPUs
+	scanner := bufio.NewScanner(bytes.NewReader(result))
+	scanner.Split(bufio.ScanLines)
+
+	re := regexp.MustCompile("GPU \\d+: (.+) \\(UUID: (.+)\\)")
+	for scanner.Scan() {
+		result := re.FindAllStringSubmatch(scanner.Text(), -1)
+		if len(result) == 1 && len(result[0]) == 3 {
+			session, err := client.NewSession()
+			if err != nil {
+				json.NewEncoder(w).Encode(JSONResponse{false, "Unable to create session"})
+				return
+			}
+
+			server.Resources = append(server.Resources, Resource{Name: result[0][1], UUID: result[0][2], InUse: false, Connection: session})
+		}
+	}
+
+	servers = append(servers, server)
 
 	json.NewEncoder(w).Encode(JSONResponse{true, string(result)})
 }
