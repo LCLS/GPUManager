c7e5399ff0a7b67821b1061e554a0384de34d03a
James Sweet <james.sweet88@googlemail.com>
2016-02-09 15:46:55 -0500

    Pulled models out into its own file

--- main.go
+++ main.go
@@ -7,10 +7,8 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 	"text/template"
@@ -154,72 +152,4 @@ func serverAddHandker(w http.ResponseWriter, r *http.Request) {
 	servers = append(servers, server)
 
 	json.NewEncoder(w).Encode(JSONResponse{Success: true, Message: string(result), Server: ServerResponse{server.Hostname, server.URL, len(server.Resources)}})
-}
-
-type Model struct {
-	Name  string
-	Files []string
-}
-
-var models []Model
-
-func modelHandler(w http.ResponseWriter, r *http.Request) {
-	type JSONResponse struct {
-		Success bool   `json:"success"`
-		Message string `json:"message"`
-	}
-
-	funcMap := template.FuncMap{
-		"join": func(in []string) string {
-			return strings.Join(in, ", ")
-		},
-	}
-
-	t, err := template.New("model.html").Funcs(funcMap).ParseFiles("model.html")
-	if err != nil {
-		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: err.Error()})
-		return
-	}
-	if err := t.Execute(w, models); err != nil {
-		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: err.Error()})
-		return
-	}
-}
-
-func modelAddHandler(w http.ResponseWriter, r *http.Request) {
-	type JSONResponse struct {
-		Success bool   `json:"success"`
-		Message string `json:"message"`
-		Model   *Model `json:"model"`
-	}
-
-	if err := r.ParseMultipartForm(1 * 1024 * 1024); err != nil {
-		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Unable to parse form", Model: nil})
-		return
-	}
-
-	name := r.FormValue("name")
-	if name == "" {
-		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Missing Name", Model: nil})
-		return
-	}
-
-	if err := os.Mkdir("data/"+name, 0755); err != nil {
-		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Unable to create folder", Model: nil})
-		return
-	}
-
-	model := Model{Name: name}
-
-	for _, fileHeaders := range r.MultipartForm.File {
-		for _, fileHeader := range fileHeaders {
-			file, _ := fileHeader.Open()
-			buf, _ := ioutil.ReadAll(file)
-			ioutil.WriteFile(fmt.Sprintf("data/%s/%s", name, fileHeader.Filename), buf, os.ModePerm)
-			model.Files = append(model.Files, fileHeader.Filename)
-		}
-	}
-
-	models = append(models, model)
-	json.NewEncoder(w).Encode(JSONResponse{Success: true, Message: "", Model: &model})
 }
