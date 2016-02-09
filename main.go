89f8c725a812c4e7060f90541ec8d9e4bad2ffb1
James Sweet <james.sweet88@googlemail.com>
2016-02-09 15:12:31 -0500

    Add uploading of models to the server

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"text/template"
@@ -34,8 +36,12 @@ func main() {
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
 	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("./fonts/"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
+
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/server/add", serverAddHandker)
+
+	http.HandleFunc("/model", modelHandler)
+	http.HandleFunc("/model/add", modelAddHandler)
 
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
@@ -135,4 +141,72 @@ func serverAddHandker(w http.ResponseWriter, r *http.Request) {
 	servers = append(servers, server)
 
 	json.NewEncoder(w).Encode(JSONResponse{Success: true, Message: string(result), Server: ServerResponse{server.Hostname, server.URL, len(server.Resources)}})
+}
+
+type Model struct {
+	Name  string
+	Files []string
+}
+
+var models []Model
+
+func modelHandler(w http.ResponseWriter, r *http.Request) {
+	type JSONResponse struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+	}
+
+	funcMap := template.FuncMap{
+		"join": func(in []string) string {
+			return strings.Join(in, ", ")
+		},
+	}
+
+	t, err := template.New("model.html").Funcs(funcMap).ParseFiles("model.html")
+	if err != nil {
+		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: err.Error()})
+		return
+	}
+	if err := t.Execute(w, models); err != nil {
+		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: err.Error()})
+		return
+	}
+}
+
+func modelAddHandler(w http.ResponseWriter, r *http.Request) {
+	type JSONResponse struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+		Model   *Model `json:"model"`
+	}
+
+	if err := r.ParseMultipartForm(1 * 1024 * 1024); err != nil {
+		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Unable to parse form", Model: nil})
+		return
+	}
+
+	name := r.FormValue("name")
+	if name == "" {
+		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Missing Name", Model: nil})
+		return
+	}
+
+	if err := os.Mkdir("data/"+name, 0755); err != nil {
+		json.NewEncoder(w).Encode(JSONResponse{Success: false, Message: "Unable to create folder", Model: nil})
+		return
+	}
+
+	model := Model{Name: name}
+
+	for _, fileHeaders := range r.MultipartForm.File {
+		for _, fileHeader := range fileHeaders {
+			file, _ := fileHeader.Open()
+			buf, _ := ioutil.ReadAll(file)
+			ioutil.WriteFile(fmt.Sprintf("data/%s/%s", name, fileHeader.Filename), buf, os.ModePerm)
+			model.Files = append(model.Files, fileHeader.Filename)
+		}
+	}
+
+	models = append(models, model)
+	json.NewEncoder(w).Encode(JSONResponse{Success: true, Message: "", Model: &model})
 }
